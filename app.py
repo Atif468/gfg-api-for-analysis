72740cb32bab9f4075d830370ab4720037a8f000
Atif468 <withatifansari@gmail.com>
2025-09-13 15:08:11 +0530

    first commit with all code

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_restful import Api, Resource
+from flask_cors import CORS  # <-- add this import
 from modules.scrap import scrap
 
 app = Flask(__name__)
+CORS(app)  # <-- enable CORS for all routes
 api = Api(app)
 
 class geeksforgeeksAPI(Resource):
