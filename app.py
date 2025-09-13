09f9cc8b53365d99bff76102eafe2c58af3dd066
Mohd Atif Ansari <114794817+Atif468@users.noreply.github.com>
2025-09-13 14:56:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_restful import Api, Resource
+from modules.scrap import scrap
+
+app = Flask(__name__)
+api = Api(app)
+
+class geeksforgeeksAPI(Resource):
+    def get(self, username):
+        scrapper = scrap(username)
+        return scrapper.fetchResponse()
+
+# register route
+api.add_resource(geeksforgeeksAPI, "/<string:username>")
+
+# ⚠️ Remove app.run() when deploying to Vercel
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000, debug=True)
